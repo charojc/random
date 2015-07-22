0ba1dbe666dc06671ec0dc0083bde0ae7e324ce7
charojc <maestracharo@gmail.com>
2015-07-22 19:19:32 +0200

    random_v1

---
+++ random.js
@@ -0,0 +1,8 @@
+//Math.random() devuelve un número aleatorio entre 0 y 1
+var n = Match.random();
+if(n<=0.5){
+	console.log('\n' + n + ' MENOR que 0,5 \n');
+}
+else{
+	console.log('\n' + n + ' MAYOR que 0,5 \n');	
+}
